541fce3bccad3400f1fbf000816d762dff565eaf
wlfpanda <wlfpanda1012@gmail.com>
2024-07-18 17:40:57 +0800

    展馆展会 init（先处理企业部门问题）

---
+++ migrations/2024_07_18_100644_create_halls_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Hyperf\Database\Schema\Schema;
+use Hyperf\Database\Schema\Blueprint;
+use Hyperf\Database\Migrations\Migration;
+
+class CreateHallsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('halls', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->addColumn('smallInteger', 'status', ['default' => 1, 'comment' => '状态 (1正常 2停用)'])->nullable();
+            $table->addColumn('smallInteger', 'sort', ['unsigned' => true, 'default' => 0, 'comment' => '排序'])->nullable();
+            $table->addColumn('bigInteger', 'created_by', ['comment' => '创建者'])->nullable();
+            $table->addColumn('bigInteger', 'updated_by', ['comment' => '更新者'])->nullable();
+            $table->timestamps();
+            $table->addColumn('timestamp', 'deleted_at', ['precision' => 0, 'comment' => '删除时间'])->nullable();
+            $table->addColumn('string', 'remark', ['length' => 255, 'comment' => '备注'])->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('halls');
+    }
+}
