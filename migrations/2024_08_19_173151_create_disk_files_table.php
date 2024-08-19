b9b422f01c00b6dbfedb712bc4b697f10766a573
wlfpanda <wlfpanda1012@gmail.com>
2024-08-19 17:40:34 +0800

    云盘表结构

---
+++ migrations/2024_08_19_173151_create_disk_files_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Hyperf\Database\Schema\Schema;
+use Hyperf\Database\Schema\Blueprint;
+use Hyperf\Database\Migrations\Migration;
+
+class CreateDiskFilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('disk_files', function (Blueprint $table) {
+            $table->engine = 'Innodb';
+            $table->comment('云盘表');
+            $table->bigIncrements('id')->comment('主键');
+            $table->addColumn('string', 'name', ['length' => 255, 'comment' => '文件(文件夹)名'])->nullable();
+            $table->addColumn('string', 'path', ['length' => 255, 'comment' => '文件(文件夹)路径'])->nullable();
+            $table->addColumn('string', 'hash', ['length' => 64, 'comment' => '文件hash'])->nullable();
+            $table->addColumn('string', 'suffix', ['length' => 10, 'comment' => '文件后缀'])->nullable();
+            $table->addColumn('bigInteger', 'parent_id', ['default' => 0,'unsigned' => true, 'comment' => '父ID'])->index();
+            $table->addColumn('bigInteger', 'size_byte', ['length' => 20, 'comment' => '字节数'])->nullable();
+            $table->addColumn('string', 'size_info', ['length' => 50, 'comment' => '文件大小'])->nullable();
+            $table->addColumn('smallInteger', 'is_folder', ['default' => 0, 'comment' => '是否文件夹'])->index()->nullable();
+            $table->addColumn('bigInteger', 'created_by', ['comment' => '创建者'])->nullable();
+            $table->addColumn('bigInteger', 'updated_by', ['comment' => '更新者'])->nullable();
+            $table->addColumn('timestamp', 'created_at', ['precision' => 0, 'comment' => '创建时间'])->nullable();
+            $table->addColumn('timestamp', 'updated_at', ['precision' => 0, 'comment' => '更新时间'])->nullable();
+            $table->addColumn('timestamp', 'deleted_at', ['precision' => 0, 'comment' => '删除时间'])->nullable();
+            $table->addColumn('string', 'remark', ['length' => 255, 'comment' => '备注'])->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('disk_files');
+    }
+}
