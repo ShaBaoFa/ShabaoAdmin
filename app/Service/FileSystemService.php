75bb449c93d692e843441cb52b7b2436fbef5817
wlfpanda1012 <wlfpanda1012@gmail.com>
2024-09-19 15:06:35 +0800

    删除利用oss预览的代码，用kkfileview去做预览功能

--- app/Service/FileSystemService.php
+++ app/Service/FileSystemService.php
@@ -12,18 +12,13 @@ declare(strict_types=1);
 
 namespace App\Service;
 
-use App\Amqp\Consumer\OssProcessConsumer;
-use App\Amqp\Producer\OssProcessProducer;
 use App\Base\BaseService;
 use App\Base\BaseUpload;
-use App\Constants\DiskFileCode;
 use App\Constants\ErrorCode;
 use App\Constants\FileSystemCode;
-use App\Constants\UploadCode;
 use App\Constants\UploadStatusCode;
 use App\Dao\UploadFileDao;
 use App\Exception\BusinessException;
-use App\Vo\AmqpQueueVo;
 use Carbon\Carbon;
 use Exception;
 use Hyperf\Cache\Annotation\Cacheable;
@@ -34,19 +29,13 @@ use Hyperf\Di\Annotation\Inject;
 use Hyperf\HttpMessage\Upload\UploadedFile;
 use League\Flysystem\Filesystem;
 use League\Flysystem\FilesystemException;
-use OSS\Core\OssException;
-use OSS\Http\RequestCore_Exception;
-use OSS\OssClient;
 use Psr\Container\ContainerExceptionInterface;
 use Psr\Container\NotFoundExceptionInterface;
 use RedisException;
 use Swoole\Coroutine\System;
-use Wlfpanda1012\AliyunSts\Constants\OSSClientCode;
 use Wlfpanda1012\AliyunSts\Oss\OssRamService;
 
-use function App\Helper\base64url_encode;
 use function App\Helper\user;
-use function Hyperf\Config\config;
 use function Hyperf\Support\make;
 
 class FileSystemService extends BaseService
@@ -144,7 +133,6 @@ class FileSystemService extends BaseService
             $sts = $this->config->get('sts');
             $ossRamService = make(OssRamService::class, ['option' => $sts]);
             $customParams = ['hash' => $hash];
-            $this->generateOssCallback($customParams);
             return ['callback_custom_params' => $customParams, 'credentials' => $ossRamService->allowPutObject($file['url'])];
         } catch (Exception $e) {
             throw new BusinessException(ErrorCode::GET_STS_TOKEN_FAIL);
@@ -214,126 +202,11 @@ class FileSystemService extends BaseService
         return $file;
     }
 
-    public function getPreview(string $hash): array
-    {
-        $file = $this->getFileInfoByHash($hash);
-        if (! Arr::get($file, 'preview_url')) {
-            [$config,$saveObj] = $this->processForPreview($file);
-            // 是否可以持久化判断
-            if ($this->canPersistence($file)){
-                // 是否需要同步持久化
-                $needSync = true;
-                if (config('amqp.enable') && di()->get(OssProcessConsumer::class)->isEnable()) {
-                    // 通过队列去处理预览持久化
-                    $amqpQueueVo = new AmqpQueueVo();
-                    $amqpQueueVo->setProducer(OssProcessProducer::class);
-                    $queueData = [
-                        'id' => Arr::get($file, 'id'),
-                        'config' => $config,
-                        'save_obj' => $saveObj,
-                        'type' => $this->checkSuffix(Arr::get($file, 'suffix')),
-                    ];
-                    $amqpQueueVo->setData($queueData);
-                    if (di()->get(QueueLogService::class)->addQueue($amqpQueueVo)) {
-                        $needSync = false;
-                    }
-                }
-                if ($needSync) {
-                    $this->saveAs($file, $config, $saveObj);
-                }
-            }
-
-            return [
-                'preview_url' => $this->generateSignature(Arr::get($file, 'url'), $config),
-                'type' => $this->checkSuffix(Arr::get($file, 'suffix')),
-                'is_persistence' => false,
-            ];
-        }
-        $config = [];
-        if ($this->checkSuffix(Arr::get($file, 'suffix')) == DiskFileCode::FILE_TYPE_VIDEO) {
-            $config = [OssClient::OSS_PROCESS => 'hls/sign,live_1'];
-        }
-        return [
-            'preview_url' => $this->generateSignature(Arr::get($file, 'preview_url'), $config),
-            'type' => $this->checkSuffix(Arr::get($file, 'suffix')),
-            'is_persistence' => true,
-        ];
-    }
-
-    /**
-     * @throws NotFoundExceptionInterface
-     * @throws RedisException
-     * @throws ContainerExceptionInterface
-     */
-    public function getThumbnail(string $hash): array
-    {
-        $file = $this->getFileInfoByHash($hash);
-        if (! Arr::get($file, 'thumb_url')) {
-            $config = $this->processForPreview($file);
-            return ['thumb_url' => $this->generateSignature(Arr::get($file, 'url'), $config)];
-        }
-        return ['thumb_url' => $this->generateSignature(Arr::get($file, 'thumb_url'))];
-    }
-
     #[CacheEvict(prefix: 'fileInfoByHash', value: 'fileHash_#{hash}')]
     public function updateByHash(string $hash, array $data): bool
     {
         $id = $this->dao->value(['hash' => $hash]);
         return parent::update($id, $data);
-    }
-
-    /**
-     * @throws OssException
-     * @throws RequestCore_Exception
-     */
-    public function saveAs(array $file, array $config, string $saveObj): bool
-    {
-        $ossConfig = config('file.storage.oss');
-        $ossClient = new OssClient(Arr::get($ossConfig, 'accessId'), Arr::get($ossConfig, 'accessSecret'), Arr::get($ossConfig, 'endpoint'));
-        $process = Arr::get($config, OssClient::OSS_PROCESS) .
-            '|sys/saveas' .
-            ',o_' . base64url_encode($saveObj) .
-            ',b_' . base64url_encode(Arr::get($ossConfig, 'bucket'));
-        if ($this->checkSuffix(Arr::get($file, 'suffix')) == DiskFileCode::FILE_TYPE_VIDEO) {
-            $saveObj .= '.m3u8';
-        }
-        return ! is_null($ossClient->processObject(Arr::get($ossConfig, 'bucket'), $this->formatOssUrl(Arr::get($file, 'url')), $process)) && $this->updateByHash(Arr::get($file, 'hash'), ['preview_url' => $this->formatLocalUrl($saveObj)]);
-    }
-
-    private function canPersistence(array $file): bool
-    {
-        // 目前 OSS 不支持文档预览的持久化
-        if (Arr::get($file, 'storage_mode') != FileSystemCode::OSS->value
-            && $this->checkSuffix(Arr::get($file, 'suffix')) != DiskFileCode::FILE_TYPE_DOCUMENT) {
-            return false;
-        }
-        return true;
-    }
-
-    private function formatOssUrl(string $url): string
-    {
-        // url 以 ‘/’ 开头则删除 /
-        return $url[0] === '/' ? substr($url, 1) : $url;
-    }
-
-    private function formatLocalUrl(string $url): string
-    {
-        return $url[0] === '/' ? $url : '/' . $url;
-    }
-
-    private function generateOssCallback(array $customParams = []): array
-    {
-        $sts = $this->config->get('sts');
-        $callback = $sts['oss']['callback'];
-        ! json_encode($callback) ?? throw new BusinessException(ErrorCode::SERVER_ERROR);
-        if (empty($customParams)) {
-            return [OSSClientCode::OSS_CALLBACK->value => json_encode($callback)];
-        }
-        $callback[OSSClientCode::OSS_CALLBACK_BODY->value] = $this->generateOssCallbackBody($customParams);
-        return [
-            OSSClientCode::OSS_CALLBACK->value => json_encode($callback),
-            OSSClientCode::OSS_CALLBACK_VAR->value => $this->generateOssCallbackVar($customParams),
-        ];
     }
 
     /**
@@ -347,132 +220,11 @@ class FileSystemService extends BaseService
         return $this->uploadTool->getFileSystem()->temporaryUrl($url, Carbon::now()->addHour(), $config);
     }
 
-    private function processForPreview(array $file): array
-    {
-        $watermark = di()->get(UserService::class)->value(['id' => Arr::get($file, $this->dao->getModel()->getDataScopeField())], 'username');
-        $mode = Arr::get($file, 'storage_mode');
-        $saveObj = $this->getPersistenceName($file);
-        $config = match ($this->checkSuffix(Arr::get($file, 'suffix'))) {
-            DiskFileCode::FILE_TYPE_IMAGE => $this->imageProcessForPreview($mode, $watermark),
-            DiskFileCode::FILE_TYPE_DOCUMENT => $this->docProcessForPreview($mode, $watermark),
-            DiskFileCode::FILE_TYPE_VIDEO => $this->videoProcessForPreview($mode),
-            default => throw new BusinessException(ErrorCode::NOT_SUPPORT)
-        };
-        return [$config, $saveObj];
-    }
-
-    private function getPersistenceName(array $file): string
-    {
-        return match ($this->checkSuffix(Arr::get($file, 'suffix'))) {
-            DiskFileCode::FILE_TYPE_VIDEO => $this->formatVideoPreviewUrl(Arr::get($file, 'url')),
-            default => $this->formatCommonPreviewUrl(Arr::get($file, 'url'))
-        };
-    }
-
-    private function formatCommonPreviewUrl(string $url): string
-    {
-        return UploadCode::PREVIEW_PREFIX->value . $url;
-    }
-
-    private function formatVideoPreviewUrl(string $url): string
-    {
-        $oUrl = UploadCode::PREVIEW_PREFIX->value . $url;
-        // 去掉后缀名
-        return substr($oUrl, 0, strrpos($oUrl, '.'));
-    }
-
-    private function checkSuffix(string $suffix): DiskFileCode
-    {
-        return match (true) {
-            in_array($suffix, $this->getImagesSuffix()) => DiskFileCode::FILE_TYPE_IMAGE,
-            in_array($suffix, $this->getDocsSuffix()) => DiskFileCode::FILE_TYPE_DOCUMENT,
-            in_array($suffix, $this->getVideosSuffix()) => DiskFileCode::FILE_TYPE_VIDEO,
-            default => throw new BusinessException(ErrorCode::NOT_SUPPORT)
-        };
-    }
-
-    private function getImagesSuffix(): array
-    {
-        return ['jpg', 'jpeg', 'png', 'gif', 'bmp'];
-    }
-
-    private function getDocsSuffix(): array
-    {
-        return ['pdf', 'doc', 'docx', 'xls', 'xlsx', 'ppt', 'pptx'];
-    }
-
-    private function getVideosSuffix(): array
-    {
-        return ['mp4', 'mkv', 'avi'];
-    }
-
-    private function imageProcessForPreview($mode, string $watermark): array
-    {
-        return match ($mode) {
-            FileSystemCode::OSS->value => [OssClient::OSS_PROCESS => 'image/auto-orient,1/interlace,1/resize,p_50/quality,q_90/watermark,text_' . base64url_encode($watermark) . ',type_d3F5LXplbmhlaQ,color_000000,size_40,rotate_156,fill_1,t_26,x_10,y_10'],
-            default => []
-        };
-    }
-
-    private function videoProcessForPreview($mode): array
-    {
-        /**
-         * 生成边转边播播放列表.
-         */
-        return match ($mode) {
-            FileSystemCode::OSS->value => [OssClient::OSS_PROCESS => 'hls/m3u8,vcodec_h264,fps_25,fpsopt_1,s_960x,sopt_1,scaletype_fit,arotate_1,crf_26,acodec_aac,ar_44100,ac_2,ab_128000,abopt_1,st_5000,initd_30000'],
-            default => []
-        };
-
-        /**
-         * 使用hls/sign签名边转边播.
-         */
-    }
-
-    private function docProcessForPreview($mode, string $watermark): array
-    {
-        return match ($mode) {
-            FileSystemCode::OSS->value => [OssClient::OSS_PROCESS => 'doc/preview,export_0,print_0,copy_0/watermark,text_' . base64url_encode($watermark) . ',color_#FFFFFF,rotate_156,size_30,t_60'],
-            default => []
-        };
-    }
-
+    /**
+     * 利用mate信息生成md5.
+     */
     private function fitterMd5Resource(array $metadata): bool|string
     {
         return json_encode(['user_id' => user()->getId(), 'size_byte' => $metadata['size_byte'], 'mime_type' => $metadata['mime_type'], 'last_modified' => $metadata['last_modified']]);
-    }
-
-    /**
-     * (不太用的上,前端的格式略微不同).
-     */
-    private function generateOssCallbackBody(?array $customParams = null): string
-    {
-        $sts = $this->config->get('sts');
-        $callback = $sts['oss']['callback'];
-        $baseParams = is_string($callback[OSSClientCode::OSS_CALLBACK_BODY->value]) ? explode(OSSClientCode::OSS_CALLBACK_SEPARATOR->value, $callback[OSSClientCode::OSS_CALLBACK_BODY->value]) : $callback[OSSClientCode::OSS_CALLBACK_BODY->value];
-        ! is_array($baseParams) && throw new BusinessException(ErrorCode::SERVER_ERROR);
-
-        // 遍历传入的数组，将其格式化为 'key=value' 的形式
-        foreach ($customParams as $key => $value) {
-            $variable = '${' . OSSClientCode::OSS_CALLBACK_CUSTOM_VAR_PREFIX->value . $key . '}';
-            $baseParams[] = "{$key}={$variable}";
-        }
-
-        // 将所有参数用 & 连接成一个字符串
-        return implode(OSSClientCode::OSS_CALLBACK_SEPARATOR->value, $baseParams);
-    }
-
-    /**
-     * (不太用的上,前端的格式略微不同).
-     * @param mixed $customParams
-     */
-    private function generateOssCallbackVar($customParams): bool|string
-    {
-        // 设置发起回调请求的自定义参数，由Key和Value组成，Key必须以枚举指定的前缀开始。
-        $var = [];
-        foreach ($customParams as $key => $value) {
-            $var[OSSClientCode::OSS_CALLBACK_CUSTOM_VAR_PREFIX->value . $key] = $value;
-        }
-        return json_encode($var);
     }
 }
