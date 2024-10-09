b43c6366a2bb63286b9c78187364fe5b20bf04bf
wlfpanda1012 <wlfpanda1012@gmail.com>
2024-10-09 10:12:59 +0800

    feat-easy-sts init

--- config/autoload/sts.php
+++ config/autoload/sts.php
@@ -2,73 +2,50 @@
 
 declare(strict_types=1);
 /**
- * This file is part of web-api.
+ * This file is part of Hyperf.
  *
- * @link     https://blog.wlfpanda1012.com/
- * @github   https://github.com/ShaBaoFa
- * @gitee    https://gitee.com/wlfpanda/web-api
- * @contact  mail@wlfpanda1012.com
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
  */
+use Wlfpanda1012\EasySts\Adapter\AliyunStsAdapterFactory;
+use Wlfpanda1012\EasySts\Adapter\CtyunStsAdapterFactory;
+use Wlfpanda1012\EasySts\Adapter\LocalStsAdapterFactory;
+
+/**
+ * This file is part of Hyperf.
+ *
+ * @see     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+
 use function Hyperf\Support\env;
 
 return [
-    'access_key_id' => env('STS_ACCESS_KEY_ID', 'LTAI4---------44pg8'),
-    'access_key_secret' => env('STS_ACCESS_KEY_SECRET', 'oibIsys6xl---------ETocTR'),
-    'role_arn' => env('STS_ROLE_ARN', 'acs:ram::xxxxxxxxxxx:role/xxx'),
-    'role_session_name' => env('STS_ROLE_SESSION_NAME', 'xxxxxxxxxxx'),
-    'endpoint' => env('STS_ENDPOINT', 'sts.cn-hangzhou.aliyuncs.com'),
-    'duration_seconds' => env('STS_DURATION_SECONDS', 3000),
-    'external_id' => env('STS_EXTERNAL_ID', 'external_id_test'),
-    /**
-     * 授予RAM用户使用OSS命令行工具访问目录mybucket/hangzhou/2014/和mybucket/hangzhou/2015/并列举目录中文件的权限.
-     *
-     * RAM用户不清楚目录中有哪些文件，可以使用OSS命令行工具或API直接获取目录信息，此场景下需要添加ListObjects权限。
-     */
-    'policy' => [
-        'Version' => '1',
-        'Statement' => [
-            [
-                'Effect' => 'Allow',
-                'Action' => [
-                    'oss:GetObject',
-                ],
-                'Resource' => [
-                    'acs:oss:*:*:mybucket/hangzhou/2014/*',
-                    'acs:oss:*:*:mybucket/hangzhou/2015/*',
-                ],
-            ],
-            [
-                'Effect' => 'Allow',
-                'Action' => [
-                    'oss:ListObjects',
-                ],
-                'Resource' => [
-                    'acs:oss:*:*:mybucket',
-                ],
-                'Condition' => [
-                    'StringLike' => [
-                        'oss:Prefix' => [
-                            'hangzhou/2014/*',
-                            'hangzhou/2015/*',
-                        ],
-                    ],
-                ],
-            ],
+    'default' => 'aliyun',
+    'adapter' => [
+        'local' => [
+            'driver' => LocalStsAdapterFactory::class,
+            'accessId' => 'username',
+            'accessSecret' => 'password',
         ],
-    ],
-    /**
-     * OSS相关.
-     */
-    'oss' => [
-        'bucket' => env('OSS_BUCKET', '*'),
-        'account_uid' => env('OSS_ACCOUNT_UID', '*'),
-        'region_id' => env('OSS_REGION_ID', '*'),
-        'callback' => [
-            'callbackUrl' => env('OSS_CALLBACK_URL', 'http://127.0.0.1:9501/callback'),
-            'callbackHost' => env('OSS_CALLBACK_HOST', '127.0.0.1'),
-            'callbackBody' => env('OSS_CALLBACK_BODY', 'filename=${object}&size=${size}&mimeType=${mimeType}&height=${imageInfo.height}&width=${imageInfo.width}'),
-            'callbackSNI' => env('OSS_CALLBACK_SNI', false),
-            'callbackBodyType' => env('OSS_CALLBACK_BODY_TYPE', 'application/x-www-form-urlencoded'),
+        'aliyun' => [
+            'driver' => AliyunStsAdapterFactory::class,
+            'accessId' => env('ALIYUN_STS_ACCESS_ID'),
+            'accessSecret' => env('ALIYUN_STS_ACCESS_SECRET'),
+            'roleArn' => env('ALIYUN_STS_ROLE_ARN', 'acs:ram::xxxxxxxxxxx:role/xxx'),
+            'roleSessionName' => env('ALIYUN_STS_ROLE_SESSION_NAME', 'xxxxxxxxxxx'),
+            'endpoint' => env('ALIYUN_STS_ENDPOINT', 'sts.cn-hangzhou.aliyuncs.com'),
+            'durationSeconds' => env('ALIYUN_STS_DURATION_SECONDS', 3600),
+            'externalId' => env('ALIYUN_STS_EXTERNAL_ID', 'external_id_test'),
+        ],
+        'ctyun' => [
+            'driver' => CtyunStsAdapterFactory::class,
+            'accessId' => env('CTYUN_STS_ACCESS_ID'),
+            'accessSecret' => env('CTYUN_STS_ACCESS_SECRET'),
         ],
     ],
 ];
