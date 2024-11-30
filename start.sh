7afcd87853bbcfd3998fa151ad177741ef1ec3e0
wlfpanda1012 <wlfpanda1012@gmail.com>
2024-11-30 20:45:16 +0800

    修改服务启动脚本

--- start.sh
+++ start.sh
@@ -1,2 +1,4 @@
 docker pull registry.cn-hangzhou.aliyuncs.com/hzkjyzz/zly-api:latest
+docker service rm zly-api_zly-api
+docker config rm zly-api_v1
 docker stack deploy -c /var/www/zly-api/deploy.yml --with-registry-auth zly-api
